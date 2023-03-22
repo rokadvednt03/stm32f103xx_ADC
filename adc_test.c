a9cc7d0a2e0c9d2e81312b940ae2e10b72609d4c
ROKAD VEDANT <rokadvedant03@gmail.com>
2023-03-22 09:45:25 +0530

    initial commit

---
+++ adc_test.c
@@ -0,0 +1,10 @@
+#include "stm32f103xx_gpio.h"
+#include "stm32f103xx_adc.h"
+#include <stdio.h>
+#include <stdint.h>
+
+int main()
+{
+
+
+}
---
+++ stm32f103xx_adc.h
@@ -0,0 +1,2 @@
+#include "stm32f10x.h"
+#include "stm32f103xx_gpio.h"
